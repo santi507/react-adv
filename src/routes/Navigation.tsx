1eb772f0704d08e3fabe7758af3b922c699b3511
Santiago Rios Samaniego <santiago.rios1758@gmail.com>
2023-11-08 06:27:01 -0500

    Componentes Lazy agregados

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, NavLink, Navigate, Route, Routes } from 'react-router-dom';
+
+import logo from "../logo.svg";
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
+
+export const Navigation = () => {
+    return (
+        <BrowserRouter>
+            <div className="main-layout">
+                <nav>
+                    <img src={logo} alt="React Logo" />
+                    <ul>
+                        <li>
+                            <NavLink to='/lazy1' className={({ isActive }) => isActive ? 'nav-active' : ''}>Lazy 1</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/lazy2' className={({ isActive }) => isActive ? 'nav-active' : ''}>Lazy 2</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/lazy3' className={({ isActive }) => isActive ? 'nav-active' : ''}>Lazy 3</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path="lazy1" element={<LazyPage1 />} />
+                    <Route path="lazy2" element={<LazyPage2 />} />
+                    <Route path="lazy3" element={<LazyPage3 />} />
+
+                    <Route path="/*" element={<Navigate to="home" replace />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    )
+}
